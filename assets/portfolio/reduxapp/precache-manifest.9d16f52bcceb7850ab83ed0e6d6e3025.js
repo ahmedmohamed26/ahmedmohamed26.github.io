5918f23b60ffb33bdcc0c3e1a1173ab787d33b36
ahmed mohamed <ahmedmohamed26.github.io>
2020-04-03 11:52:29 +0200

    second commit

---
+++ assets/portfolio/reduxapp/precache-manifest.9d16f52bcceb7850ab83ed0e6d6e3025.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8542a8a51f049f23d91c",
+    "url": "/static/css/main.d5fab753.chunk.css"
+  },
+  {
+    "revision": "8542a8a51f049f23d91c",
+    "url": "/static/js/main.e51a17fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1fafc414dcfda94abb40",
+    "url": "/static/js/2.fa9cf39f.chunk.js"
+  },
+  {
+    "revision": "0b15e74013436d937896a337bc9ae946",
+    "url": "/index.html"
+  }
+];
